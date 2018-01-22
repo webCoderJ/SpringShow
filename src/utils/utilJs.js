0b26d1e69723e2b678c3f75255a953f78b218710
pengjie <pengjie@tigerwit.com>
2018-01-22 19:09:33 +0800

    MOD

--- src/utils/utilJs.js
+++ src/utils/utilJs.js
@@ -10,24 +10,36 @@ let touchInfo = (e) => {
   // console.log('____touchInfo____', touch)
   return touch
 }
-
+// 两点距离
+let distance = (x1, y1, x2, y2) => Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2))
 // 对角线
 let string = (w, h) => Math.sqrt(w * w + h * h)
 // 根据对角线与勾股比计算宽度
 let stringWH = (slicer) => {
+  slicer.string = string(
+    slicer.curX,
+    slicer.curY,
+    slicer.centerX,
+    slicer.centerY
+  );
   // whRatio
-  let h = Math.sqrt(Math.pow(slicer.string, 2) / (1 + Math.pow(slicer.whRatio, 2)))
+  let w = Math.sqrt(Math.pow(slicer.string, 2) / (1 + Math.pow(slicer.whRatio, 2)))
   return {
-    w: h / slicer.whRatio,
-    h: h
+    string: slicer.string,
+    h: w / slicer.whRatio,
+    w: w
   }
 }
-// 两点距离
-let distance = (x1, y1, x2, y2) => Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2))
+
+let scale = (ox, oy, x1, y1, x2, y2) => {
+  // console.log('scale', distance(ox, oy, x1, y1) / distance(ox, oy, x2, y2))
+  return distance(ox, oy, x1, y1) / distance(ox, oy, x2, y2)
+}
+
 // 返回余弦角度
 let cosDeg = (a, b, c) => {
-  console.log(Math.acos((a * a + b * b - c * c) / (2 * a * b)))
-  return (Math.PI) * Math.acos((a * a + b * b - c * c) / (2 * a * b))
+  console.log('旋转角度...', (180 / Math.PI) * Math.acos((a * a + b * b - c * c) / (2 * a * b)))
+  return (180 / Math.PI) * Math.acos((a * a + b * b - c * c) / (2 * a * b))
 }
 function deepClone(obj) {
   var newObj
@@ -53,6 +65,7 @@ module.exports = {
   touchInfo,
   string,
   stringWH,
+  scale,
   distance,
   cosDeg,
   deepClone
