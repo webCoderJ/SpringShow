0ceb66dbfd895150cc490d172ed0974b933f4da8
pengjie <pengjie@tigerwit.com>
2018-01-23 19:12:41 +0800

    MOD

--- src/utils/utilJs.js
+++ src/utils/utilJs.js
@@ -12,6 +12,11 @@ let touchInfo = (e) => {
 }
 // 两点距离
 let distance = (x1, y1, x2, y2) => Math.sqrt(Math.pow((x1 - x2), 2) + Math.pow((y1 - y2), 2))
+let radianToDeg = (rad) => (180 / Math.PI) * rad
+// 两点角度
+let atan2Radian = (ox, oy, x, y) => {
+  return Math.atan2( y - oy, x - ox )
+}
 // 相对某点的相对坐标
 let realCoor = (ox, oy, x, y) => [ x - ox, y - oy]
 // 对角线
@@ -34,19 +39,19 @@ let stringWH = (slicer) => {
 }
 let scale = (ox, oy, x1, y1, x2, y2) => {
   // console.log('scale', distance(ox, oy, x1, y1) / distance(ox, oy, x2, y2))
-  return distance(ox, oy, x1, y1) / distance(ox, oy, x2, y2)
+  return distance(ox, oy, x2, y2) / distance(ox, oy, x1, y1)
 }
 // 返回余弦角度
 let cosDeg = (a, b, c) => {
   console.log('[旋转角度]', (180 / Math.PI) * Math.acos((a * a + b * b - c * c) / (2 * a * b)))
   return (180 / Math.PI) * Math.acos((a * a + b * b - c * c) / (2 * a * b))
 }
-
 // 返回两个坐标点的夹角
 let angle = (x1, y1, x2, y2) => {
   console.log('[旋转角度]', Math.abs(Math.atan(y2 / x2) - Math.atan(y1 / x1)) * (180 / Math.PI))
   return (Math.abs(Math.atan(y2 / x2) - Math.atan(y1 / x1))) * (180 / Math.PI)
 }
+
 function deepClone(obj) {
   var newObj
   if (obj instanceof Array) {
@@ -76,5 +81,7 @@ module.exports = {
   cosDeg,
   deepClone,
   realCoor,
-  angle
+  angle,
+  atan2Radian,
+  radianToDeg
 }
